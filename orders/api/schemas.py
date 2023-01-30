806cacbef22172ea62b93a61ff400595e38d1f12
Suryana <nanasuryana335@gmail.com>
2023-01-30 17:06:27 +0700

    feat: add model schemas to validating request payload

---
+++ orders/api/schemas.py
@@ -0,0 +1,39 @@
+from enum import Enum
+from typing import List, Optional
+from uuid import UUID
+
+from pydantic import BaseModel, Field, conlist, conint
+
+
+class Size(Enum):
+    small = 'small'
+    medium = 'medium'
+    big = 'big'
+
+
+class Status(Enum):
+    created = 'created'
+    progress = 'progress'
+    cancelled = 'cancelled'
+    dispatched = 'dispatched'
+    delivered = 'delivered'
+
+
+class OrderItemSchema(BaseModel):
+    product: str
+    size: Size
+    quantity: conint[Optional(ge=1, strict=True)] = 1
+
+
+class CreateOrderSchema(BaseModel):
+    order: conlist(OrderItemSchema, min_items=1)
+
+
+class GetOrderSchema(CreateOrderSchema):
+    id: UUID
+    created: datetime
+    status: Status
+
+
+class GetOrdersSchema(BaseModel):
+    orders: List[GetOrderSchema]

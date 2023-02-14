475718e2329d44304e5d011b525d08291d0d8dbf
Suryana <nanasuryana335@gmail.com>
2023-02-14 23:25:32 +0700

    feat: override api docs with my specification
    Here's several important point of this commit:
    
    [1] Import the yaml module >> first, i install PyYAML to do that
    [2] Load the API specification using PyYAML using yaml.safe_load
    [3] Override FastAPI's openapi property using my own specification

--- orders/app.py
+++ orders/app.py
@@ -1,4 +1,15 @@
+from pathlib import Path
 from fastapi import FastAPI
-from orders.api import api
 
-app = FastAPI(debug=True)
+import yaml
+
+app = FastAPI(debug=True, openapi_url='/openapi/orders.json',
+              docs_url='/docs/orders')
+
+oas_doc = yaml.safe_load(
+    (Path(__file__).parent / '../oas.yaml').read_text()
+)
+
+app.open_api = lambda: oas_doc
+
+from orders.api import api

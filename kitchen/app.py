4c3976a44c3f4157f1517923c0cb64c86ae6691f
Suryana <nanasuryana335@gmail.com>
2023-02-26 21:38:51 +0700

    feat: override the flask_smorest's generated API specification

--- kitchen/app.py
+++ kitchen/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_smorest import Api
 
+from pathlib import Path
+from apispec import APISpec
+import yaml
+
 from config import BaseConfig
 
 from api.api import blueprint
@@ -10,3 +14,12 @@ app.config.from_object(BaseConfig)
 
 kitchen_api = Api(app)
 kitchen_api.register_blueprint(blueprint)
+
+api_spec = yaml.safe_load((Path(__file__).parent / "oas.yaml").read_text())
+spec = APISpec(
+    title=api_spec["info"]["title"],
+    version=api_spec["info"]["version"],
+    openapi_version=api_spec["openapi"],
+)
+spec.to_dict = lambda: api_spec
+kitchen_api.spec = spec

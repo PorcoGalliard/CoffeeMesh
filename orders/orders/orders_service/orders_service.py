89677503750e60c9de0d4df81c9d459d5adb4319
Suryana <nanasuryana335@gmail.com>
2023-03-06 11:54:02 +0700

    feat: add additional information and implementation
    detail:
    
    [1] add additional information about OrdersService class
    [2] when initializing OrdersService class, i need orders_repository instance as parameter with OrdersRepository class hint
    [3] implement the place_order interface using orders_repository.add() method

--- orders/orders/orders_service/orders_service.py
+++ orders/orders/orders_service/orders_service.py
@@ -1,14 +1,16 @@
 # IMPLEMENT THE INTERFACE OF ORDER SERVICE CLASS
 
 from repository.orders_repository import OrdersRepository
+from orders_service.exceptions import OrderNotFoundError
 
 
 class OrdersService:
-    def __init__(self, orders_repository):
+    def __init__(self, orders_repository: OrdersRepository):
+        # To instantiate the order service class, required orders_repository instance
         self.orders_repository = orders_repository
 
     def place_order(self, items):
-        pass
+        return self.orders_repository.add(items)
 
     def get_order(self, order_id):
         pass

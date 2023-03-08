fecc4bce46fc14b8239a06bae8436d29211b3d49
Suryana <nanasuryana335@gmail.com>
2023-03-08 22:27:47 +0700

    feat: implement the __enter__ context manager attribute

--- orders/orders/repository/unit_of_work.py
+++ orders/orders/repository/unit_of_work.py
@@ -4,6 +4,13 @@ from sqlalchemy.orm import sessionmaker
 
 class UnitOfWork:
     def __init__(self):
+        # obtain the session factory object
         self.session_maker = sessionmaker(
             bind=create_engine('sqlite:///orders.db')
         )
+
+    def __enter__(self):
+        # open new database session
+        self.session = self.session_maker()
+        # return the instance of unit of work object
+        return self

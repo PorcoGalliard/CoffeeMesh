c54a005ade9118c237d1e6d5f109524186833416
Suryana <nanasuryana335@gmail.com>
2023-03-23 20:51:37 +0700

    feat: add query instance to schema object

--- products/web/schema.py
+++ products/web/schema.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 from ariadne import make_executable_schema
 
+from web.queries import query
+
 schema = make_executable_schema(
-    (Path(__file__).parent / 'products.graphql').read_text()
+    (Path(__file__).parent / 'products.graphql').read_text(), [query]
 )

a3910a14ac2779a6415b7c1d1e961ff8a41d6034
Suryana <nanasuryana335@gmail.com>
2023-03-23 21:35:40 +0700

    feat: create products resolver (implement query parameter)

--- products/web/queries.py
+++ products/web/queries.py
@@ -13,3 +13,34 @@ def resolve_all_ingredients(*_):
 @query.field('allProducts')
 def resolve_all_products(*_):
     return products
+
+
+@query.field('products')
+def resolve_products(*_, input=None):
+    filtered = [product for product in products]
+    if input is None:
+        return filtered
+
+    filtered = [
+        product for product in filtered
+        if product['available'] is input['available']
+    ]
+
+    if input.get('minPrice') is not None:
+        filtered = [
+            product for product in filtered
+            if product['price'] >= input['minPrice']
+        ]
+
+    if input.get('maxPrice') is not None:
+        filtered = [
+            product for product in products
+            if product['maxPrice'] <= input['maxPrice']
+        ]
+
+    filtered.sort(
+        key=lambda product: product.get(input['sortBy'], 0),
+        reverse=input['sort'] == 'DESCENDING'
+    )
+
+    return filtered

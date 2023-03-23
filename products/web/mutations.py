473fcd083478534cb7bc82fe033fb8d8632bc499
Suryana <nanasuryana335@gmail.com>
2023-03-23 22:14:17 +0700

    feat: implement mutation resolver of addProduct

---
+++ products/web/mutations.py
@@ -0,0 +1,33 @@
+from ariadne import MutationType
+
+import uuid
+from datetime import datetime
+
+from web.data import products
+
+mutation = MutationType()
+
+
+@mutation.field('addProduct')
+def resolve_add_product(*_, name, type, input):
+    product = {
+        'id': uuid.uuid4(),
+        'name': name,
+        'available': input.get('available', False),
+        'ingredients': input.get('ingredients', []),
+        'lastUpdated': datetime.utcnow()
+    }
+
+    if type == 'Cake':
+        product.update({
+            'hasFilling': input['hasFilling'],
+            'hasNutsToppingOption': input['hasNutsToppingOption']
+        })
+    else:
+        product.update({
+            'hasCreamOnTopOption': input['hasCreamOnTopOption'],
+            'hasServeOnIceOption': input['hasServeOnIceOption'],
+        }
+        )
+    products.append(product)
+    return product
